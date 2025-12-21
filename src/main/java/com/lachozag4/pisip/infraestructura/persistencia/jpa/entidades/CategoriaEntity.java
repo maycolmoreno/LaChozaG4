d2ed3d1db5b9cf7c9eb8ea0b23e4e38af44b0c47
Ronald Moreno <maycol9ronald99@gmail.com>
2025-12-20 20:49:51 -0500

    Categoria

---
+++ src/main/java/com/lachozag4/pisip/infraestructura/persistencia/jpa/entidades/CategoriaEntity.java
@@ -0,0 +1,35 @@
+package com.lachozag4.pisip.infraestructura.persistencia.jpa.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @OneToMany(
+        mappedBy = "categoria",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
+    private List<ProductoEntity> productos;
+}
